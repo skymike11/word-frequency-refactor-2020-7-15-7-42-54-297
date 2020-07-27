dc11b130e2cfdb9a5867f861b2be7937ff83a1f1
OOCLDM\lisa8 <lisa8@oocl.com>
2020-07-27 15:30:01 +0800

    [Word Frequency Refactoring][Sam] refactor: use stream to instead of loop to make map->list and sort

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -1,8 +1,8 @@
 import java.util.*;
-import java.io.CharArrayWriter;
 
-import java.time.LocalDateTime;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.reducing;
 
 public class WordFrequencyGame {
     public String getResult(String sentence) {
@@ -13,7 +13,7 @@ public class WordFrequencyGame {
     }
 
     private String formatString(Map<String, Integer> wordMap) {
-        List<Input> list = wordMap.entrySet().stream().sorted((entry1, entry2)-> entry2.getValue() - entry1.getValue())
+        List<Input> list = wordMap.entrySet().stream().sorted((entry1, entry2) -> entry2.getValue() - entry1.getValue())
                 .map(word -> new Input(word.getKey(), word.getValue())).collect(Collectors.toList());
 
         return list.stream().map(word -> word.getValue() + " " + word.getWordCount()).collect(Collectors.joining("\n"));
@@ -22,29 +22,7 @@ public class WordFrequencyGame {
     private Map<String, Integer> getWordMapBySentence(String sentence) {
         String[] words = sentence.split("\\s+");
 
-        Map<String, Integer> wordMap = new HashMap<>();
-        for (String word : words) {
-            if (wordMap.containsKey(word)) {
-                wordMap.put(word, wordMap.get(word) + 1);
-            } else {
-                wordMap.put(word, 1);
-            }
-        }
-        return wordMap;
+        return Arrays.stream(words).collect(Collectors.groupingBy(item -> item, reducing(0, e -> 1, Integer::sum)));
     }
 
-    private Map<String, List<Input>> getListMap(List<Input> inputList) {
-        Map<String, List<Input>> map = new HashMap<>();
-        for (Input input : inputList) {
-//       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
-            if (!map.containsKey(input.getValue())) {
-                ArrayList arr = new ArrayList<>();
-                arr.add(input);
-                map.put(input.getValue(), arr);
-            } else {
-                map.get(input.getValue()).add(input);
-            }
-        }
-        return map;
-    }
 }
